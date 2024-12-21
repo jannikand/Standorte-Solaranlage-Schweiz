dc2240e5645eba1faa57b84b8a7367fbea67f3c0
Jannik Anderegg <jannik.anderegg@gmail.com>
2024-12-21 14:10:57 +0100

    v7

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,8 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
 import { useState } from "react";
-import { Box } from "@mui/material";
-import Button from "@mui/material/Button";
-import Alert from "@mui/material/Alert";
+import { Box, Button, Alert, TextField, Stack, Divider } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 import { VegaLite } from "react-vega";
 import standard_visualisierung from "/public/standard_visualisierung.json";
 
@@ -16,15 +15,18 @@ export default function App() {
   const [Sonne_min, setSonne_min] = useState(0.01);
   const [Sonne_max, setSonne_max] = useState(1);
   const [Visualisierung, setVisualisierung] = useState({});
+  const [isLoading, setLoading] = useState(false);
   const [ErrorMsg, setErrorMsg] = useState(null);
 
   async function FilterAnfrage() {
     let URL = `/api/standorte?ausrichtung_min=${Ausrichtung_min}&ausrichtung_max=${Ausrichtung_max}&neigung_min=${Neigung_min}&neigung_max=${Neigung_max}&sonne_min=${Sonne_min}&sonne_max=${Sonne_max}`;
     try {
+      setLoading(true);
       const resp = await fetch(URL);
       if (resp.ok) {
         const data = await resp.json();
         setVisualisierung(data);
+        setLoading(false);
       } else {
         throw new Error("Fehler beim Abrufen der Daten");
       }
@@ -35,18 +37,60 @@ export default function App() {
 
   return (
     <>
-      <Button variant="contained" onClick={FilterAnfrage}>
-        Filter anwenden
-      </Button>
-      {ErrorMsg && <Alert severity="error">{ErrorMsg}</Alert>}
-      <Box
+      <Stack
+        direction="column"
+        spacing={2}
         sx={{
-          display: "flex",
           justifyContent: "center",
+          alignItems: "center",
+          padding: 2,
         }}
       >
+        <Stack
+          direction="column"
+          spacing={2}
+          sx={{
+            alignItems: "flex-start",
+            width: "100%",
+            maxWidth: "1330px",
+          }}
+        >
+          <Typography variant="h2">Standorte Grosssolaranlagen</Typography>
+          <Divider orientation="horizontal" flexItem />
+          <Typography variant="subtitle1">
+            Filter der Standorte anpassen
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{
+              alignItems: "flex-start",
+            }}
+          >
+            <TextField
+              label="Minimale Ausrichtung"
+              value={Ausrichtung_min}
+              onChange={(e) => setAusrichtung_min(e.target.value)}
+              type="number"
+            />
+            <TextField
+              label="Maximale Ausrichtung"
+              value={Ausrichtung_max}
+              onChange={(e) => setAusrichtung_max(e.target.value)}
+              type="number"
+            />
+          </Stack>
+
+          <Button variant="contained" color="secondary" onClick={FilterAnfrage}>
+            Filter anwenden
+          </Button>
+          <Divider orientation="horizontal" flexItem />
+        </Stack>
+
+        {isLoading && <CircularProgress color="secondary" />}
+
         <VegaLite spec={Visualisierung} />
-      </Box>
+      </Stack>
     </>
   );
 }
