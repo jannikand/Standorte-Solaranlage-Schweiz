fa475aed463b68c5d938cc1c08c6824325c51eaa
Jannik Anderegg <jannik.anderegg@gmail.com>
2024-12-21 12:44:00 +0100

    v6.2

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,17 @@ import { VegaLite } from "react-vega";
 import standard_visualisierung from "/public/standard_visualisierung.json";
 
 export default function App() {
+  const [Ausrichtung_min, setAusrichtung_min] = useState(140);
+  const [Ausrichtung_max, setAusrichtung_max] = useState(220);
+  const [Neigung_min, setNeigung_min] = useState(10);
+  const [Neigung_max, setNeigung_max] = useState(50);
+  const [Sonne_min, setSonne_min] = useState(0.01);
+  const [Sonne_max, setSonne_max] = useState(1);
   const [Visualisierung, setVisualisierung] = useState({});
   const [ErrorMsg, setErrorMsg] = useState(null);
 
   async function FilterAnfrage() {
-    let URL = `/api/standorte?ausrichtung_min=140&ausrichtung_max=220&neigung_min=10&neigung_max=50&sonne_min=0.5&sonne_max=1`;
+    let URL = `/api/standorte?ausrichtung_min=${Ausrichtung_min}&ausrichtung_max=${Ausrichtung_max}&neigung_min=${Neigung_min}&neigung_max=${Neigung_max}&sonne_min=${Sonne_min}&sonne_max=${Sonne_max}`;
     try {
       const resp = await fetch(URL);
       if (resp.ok) {
